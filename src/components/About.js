b2b19cc601e0db61ca7a8c7c3bb85cce6fbd902d
riky moroni <riky.moroni5@gmail.com>
2022-03-24 15:08:18 +0000

    add about component and styling

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import './About.css'
+
+export default function About() {
+  return (
+    <section className="about">
+      <h3>About</h3>
+      <p>I am a full-stack developer with a great interest in buildingamazing UI, but also dealing with the server and databases. I like toassist throughout the whole developement process. I enjoy learningsomething new every day and take on new challenges.</p>
+    </section>
+  )
+}
