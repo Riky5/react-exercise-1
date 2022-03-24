8eb2754c339eb0c95b837f3e045a813eed2dfb92
riky moroni <riky.moroni5@gmail.com>
2022-03-24 17:15:06 +0000

    add media query for device with screen size less than 500px

--- src/components/Interest.js
+++ src/components/Interest.js
@@ -2,7 +2,7 @@ import './Interest.css'
 
 export default function Interest() {
   return (
-    <section className="interest">
+    <section className="interests">
       <h3>Interests</h3>
       <p>Globe Trotter. Portrait Artist. Youtube Fanatic. Coffee Crafter.</p>
     </section>
